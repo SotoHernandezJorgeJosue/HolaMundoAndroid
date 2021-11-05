912c61ff8a1048567609b7b5f17b9474d45e08ff
SotoHernandezJorgeJosue <soto.hernandez.jorge.josue@gmail.com>
2021-11-05 12:58:50 -0600

    Calculadora

---
+++ app/src/main/java/com/jorgejosue/soto/hernandez/Calculadora.java
@@ -0,0 +1,16 @@
+package com.jorgejosue.soto.hernandez;
+
+public class Calculadora {
+    public static void suma() {
+
+    }
+    public static void resta() {
+
+    }
+    public static void multiplicacion() {
+
+    }
+    public static void division() {
+
+    }
+}

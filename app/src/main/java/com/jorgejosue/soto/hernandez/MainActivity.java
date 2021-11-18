b7285b56163bee0f449a7c3907a4b9b1bdbbe8e3
SotoHernandezJorgeJosue <soto.hernandez.jorge.josue@gmail.com>
2021-11-18 07:57:19 -0600

    Resta

--- app/src/main/java/com/jorgejosue/soto/hernandez/MainActivity.java
+++ app/src/main/java/com/jorgejosue/soto/hernandez/MainActivity.java
@@ -2,13 +2,44 @@ package com.jorgejosue.soto.hernandez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSuma;
+    private Button buttonResta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonSuma = findViewById(R.id.buttonSuma );
+        buttonSuma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirSuma( );
+            }
+        });
+        buttonResta = findViewById(R.id.buttonResta );
+        buttonResta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirResta( );
+            }
+        });
     }
+
+    private void abrirSuma() {
+        Intent intent = new Intent( this , SumaActivity.class );
+        startActivity( intent );
+    }
+
+    private void abrirResta() {
+        Intent intent = new Intent( this , RestaActivity.class );
+        startActivity( intent );
+    }
+
 }
